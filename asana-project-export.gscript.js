8a78ce9e0ead480bcf4eb22f2d8997d44dcbc9b6
Caelan <caelan.borowiec@pqi.org>
2019-01-16 15:07:41 -0500

    Added GetProjectsFromTeams

--- asana-project-export.gscript.js
+++ asana-project-export.gscript.js
@@ -9,7 +9,14 @@ var workspace = "";
 // Simulates PHP's date function
 Date.prototype.format=function(e){var t="";var n=Date.replaceChars;for(var r=0;r<e.length;r++){var i=e.charAt(r);if(r-1>=0&&e.charAt(r-1)=="\\"){t+=i}else if(n[i]){t+=n[i].call(this)}else if(i!="\\"){t+=i}}return t};Date.replaceChars={shortMonths:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],longMonths:["January","February","March","April","May","June","July","August","September","October","November","December"],shortDays:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],longDays:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],d:function(){return(this.getDate()<10?"0":"")+this.getDate()},D:function(){return Date.replaceChars.shortDays[this.getDay()]},j:function(){return this.getDate()},l:function(){return Date.replaceChars.longDays[this.getDay()]},N:function(){return this.getDay()+1},S:function(){return this.getDate()%10==1&&this.getDate()!=11?"st":this.getDate()%10==2&&this.getDate()!=12?"nd":this.getDate()%10==3&&this.getDate()!=13?"rd":"th"},w:function(){return this.getDay()},z:function(){var e=new Date(this.getFullYear(),0,1);return Math.ceil((this-e)/864e5)},W:function(){var e=new Date(this.getFullYear(),0,1);return Math.ceil(((this-e)/864e5+e.getDay()+1)/7)},F:function(){return Date.replaceChars.longMonths[this.getMonth()]},m:function(){return(this.getMonth()<9?"0":"")+(this.getMonth()+1)},M:function(){return Date.replaceChars.shortMonths[this.getMonth()]},n:function(){return this.getMonth()+1},t:function(){var e=new Date;return(new Date(e.getFullYear(),e.getMonth(),0)).getDate()},L:function(){var e=this.getFullYear();return e%400==0||e%100!=0&&e%4==0},o:function(){var e=new Date(this.valueOf());e.setDate(e.getDate()-(this.getDay()+6)%7+3);return e.getFullYear()},Y:function(){return this.getFullYear()},y:function(){return(""+this.getFullYear()).substr(2)},a:function(){return this.getHours()<12?"am":"pm"},A:function(){return this.getHours()<12?"AM":"PM"},B:function(){return Math.floor(((this.getUTCHours()+1)%24+this.getUTCMinutes()/60+this.getUTCSeconds()/3600)*1e3/24)},g:function(){return this.getHours()%12||12},G:function(){return this.getHours()},h:function(){return((this.getHours()%12||12)<10?"0":"")+(this.getHours()%12||12)},H:function(){return(this.getHours()<10?"0":"")+this.getHours()},i:function(){return(this.getMinutes()<10?"0":"")+this.getMinutes()},s:function(){return(this.getSeconds()<10?"0":"")+this.getSeconds()},u:function(){var e=this.getMilliseconds();return(e<10?"00":e<100?"0":"")+e},e:function(){return"Not Yet Supported"},I:function(){var e=null;for(var t=0;t<12;++t){var n=new Date(this.getFullYear(),t,1);var r=n.getTimezoneOffset();if(e===null)e=r;else if(r<e){e=r;break}else if(r>e)break}return this.getTimezoneOffset()==e|0},O:function(){return(-this.getTimezoneOffset()<0?"-":"+")+(Math.abs(this.getTimezoneOffset()/60)<10?"0":"")+Math.abs(this.getTimezoneOffset()/60)+"00"},P:function(){return(-this.getTimezoneOffset()<0?"-":"+")+(Math.abs(this.getTimezoneOffset()/60)<10?"0":"")+Math.abs(this.getTimezoneOffset()/60)+":00"},T:function(){var e=this.getMonth();this.setMonth(0);var t=this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/,"$1");this.setMonth(e);return t},Z:function(){return-this.getTimezoneOffset()*60},c:function(){return this.format("Y-m-d\\TH:i:sP")},r:function(){return this.toString()},U:function(){return this.getTime()/1e3}}
 
-var orgTeams = GetTeams(workspace);
+function TestFunc()
+{
+	var orgTeams = GetTeams(workspace);
+	Logger.log("Teams:\n" + orgTeams + "\n\n");
+	var orgProjects = GetProjectsFromTeams(orgTeams);
+	Logger.log("Projects:\n" + orgProjects + "\n\n");
+	WriteProjectDetails(orgProjects);
+}
 
 function GetTeams(workspace)
 {
@@ -48,22 +55,17 @@ function GetTeams(workspace)
 	return teams;
 }
 
-
-function GetProjects()
+function GetProjectsFromTeams(arrTeams)
 {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName('Live source data');
-
   var projects = [];
   var offset = "";
   var options = { "method" : "get"};
   options.headers = AuthData();
   options.headers["Asana-Fast-Api"] = "true";
 
-  var x = 0;
-  do
-  {
-    var urlpath = "https://app.asana.com/api/1.0/projects?limit=100&workspace=" + workspace + "&archived=false";
+	for(var i in arrTeams)
+	{
+    var urlpath = "https://app.asana.com/api/1.0/teams/" + arrTeams[i] + "/projects?archived=false&limit=100";
     if (offset != null && offset !== '')
       urlpath += "&offset=" + offset;
 
@@ -77,18 +79,22 @@ function GetProjects()
 
     var data = jResponse.data;
 
-    for(var i in data)
+    for(var x in data)
     {
-      projects.push(data[i].id);
+      projects.push(data[x].id);
     }
-    x++;
-  } while (offset != '');
+  }
 
-  Logger.log("Projects:\n" + projects);
+	return projects;
+}
 
-  //throw new Error("Got all the projects! :D");
 
-  var rangesToClear = ['A3:K1000'];
+function WriteProjectDetails(projects)
+{
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName('Live source data');
+
+  var rangesToClear = ['A3:K5000'];
   for (var i=0; i<rangesToClear.length; i++) {
 	   sheet.getRange(rangesToClear[i]).clearContent();
   }
